75a7a926a46ca3e1cf94d4f953b9cbf4513194ac
Dhanuka Lakshan <danuka1999lakshan@gmail.com>
2023-02-06 19:51:51 +0530

    commit

---
+++ web5JS/script.js
@@ -0,0 +1,16 @@
+/*
+function f1(){
+     document.getElementById("p1").className= "y";
+}*/
+
+/*function f1(){
+    document.getElementById("p1").style.fontSize="100px";
+    document.getElementById("p1").style.color = "yellow";
+}*/
+
+function f1(){
+    document.getElementById("y").style.marginLeft = "500px";
+    document.getElementById("y").style.backgroundColor = "black";
+    document.getElementById("x").style.marginLeft = "500px";
+    document.getElementById("x").innerHTML = "500px";
+}
